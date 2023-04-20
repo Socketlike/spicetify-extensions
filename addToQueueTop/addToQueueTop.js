c646acdcb07cc1125ef174706e55b05529c1c1f6
Socketlike <36399055+Socketlike@users.noreply.github.com>
2023-04-20 10:18:58 +0700

    fix(types): expose context menu option to more relevant types

--- addToQueueTop/addToQueueTop.js
+++ addToQueueTop/addToQueueTop.js
@@ -1,36 +1,44 @@
 (async function addToQueueTop() {
-	if (!(Spicetify.Platform && Spicetify.CosmosAsync)) {
-		setTimeout(addToQueueTop, 300);
-		return;
-	}
+  if (!(Spicetify.Platform && Spicetify.CosmosAsync)) {
+    setTimeout(addToQueueTop, 300);
+    return;
+  }
 
-	const { Type } = Spicetify.URI;
-	const shouldShowOption = (uris) =>
-		uris.every((uri) =>
-			[Type.TRACK, Type.PLAYLIST, Type.PLAYLIST_V2].includes(Spicetify.URI.fromString(uri).type),
-		);
+  const { Type } = Spicetify.URI;
+  const shouldShowOption = (uris) =>
+    uris.every((uri) =>
+      [
+        Type.TRACK,
+        Type.PLAYLIST,
+        Type.PLAYLIST_V2,
+        Type.ALBUM,
+        Type.LOCAL,
+        Type.EPISODE,
+        Type.SHOW,
+      ].includes(Spicetify.URI.fromString(uri).type),
+    );
 
-	new Spicetify.ContextMenu.Item(
-		"Add to queue's top",
-		async (uris) => {
-			if ([Type.PLAYLIST, Type.PLAYLIST_V2].includes(Spicetify.URI.fromString(uris[0]).type))
-				uris = (await Spicetify.Platform.PlaylistAPI.getContents(uris[0])).items.map(
-					(item) => item.uri,
-				);
-			const before = (Spicetify.Queue.nextTracks || []).filter(
-				(track) => track.provider !== 'context',
-			).length;
-			await Spicetify.addToQueue(uris.map((uri) => ({ uri })));
-			const difference = Spicetify.Queue.nextTracks
-				.filter((track) => track.provider !== 'context')
-				.filter((_, index) => index >= before);
-			if (before)
-				await Spicetify.Platform.PlayerAPI.reorderQueue(
-					difference.map((track) => track.contextTrack),
-					{ before: Spicetify.Queue.nextTracks[0].contextTrack },
-				);
-			Spicetify.showNotification('Added to top of Queue');
-		},
-		shouldShowOption,
-	).register();
+  new Spicetify.ContextMenu.Item(
+    "Add to queue's top",
+    async (uris) => {
+      if ([Type.PLAYLIST, Type.PLAYLIST_V2].includes(Spicetify.URI.fromString(uris[0]).type))
+        uris = (await Spicetify.Platform.PlaylistAPI.getContents(uris[0])).items.map(
+          (item) => item.uri,
+        );
+      const before = (Spicetify.Queue.nextTracks || []).filter(
+        (track) => track.provider !== 'context',
+      ).length;
+      await Spicetify.addToQueue(uris.map((uri) => ({ uri })));
+      const difference = Spicetify.Queue.nextTracks
+        .filter((track) => track.provider !== 'context')
+        .filter((_, index) => index >= before);
+      if (before)
+        await Spicetify.Platform.PlayerAPI.reorderQueue(
+          difference.map((track) => track.contextTrack),
+          { before: Spicetify.Queue.nextTracks[0].contextTrack },
+        );
+      Spicetify.showNotification('Added to top of Queue');
+    },
+    shouldShowOption,
+  ).register();
 })();
